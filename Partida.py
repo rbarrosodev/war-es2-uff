9038e6a77a72ee6144f67a1cf395d98fb8ee3923
Rodrigo Barroso <digorbarroso@gmail.com>
2022-11-26 15:13:30 -0300

    wip interface integration

--- Partida.py
+++ Partida.py
@@ -1,4 +1,4 @@
-#from asyncio.windows_events import NULL
+# from asyncio.windows_events import NULL
 from pickle import TRUE
 import random
 
@@ -9,156 +9,158 @@ import Player
 import CartaObj
 from territorio import territorio
 
-CORES = [ "branco" , "preto" , "vermelho" , "azul", "amarelo" , "verde" ]
-class partida:
-    def __init__(self ,n_humans, n_players = 6 ):
+CORES = ["branco", "preto", "vermelho", "azul", "amarelo", "verde"]
 
-        self.n_players  = min( n_players , 6 ) 
-        self.n_humans   = min( n_humans , n_players )  # num de players
-        self.n_npcs     = n_players - n_humans         # num de ias
+
+class partida:
+    def __init__(self, n_humans, cores_disponiveis, cores_escolhidas, n_players=6):
+        self.n_players = min(n_players, 6)
+        self.n_humans = min(n_humans, n_players)  # num de players
+        self.n_npcs = n_players - n_humans  # num de ias
         self.num_rodada = 0
         self.v_player = []
         self.npcs = []
-        #players + npc
+        self.cores_disponiveis = cores_disponiveis
+        self.cores_escolhidas = cores_escolhidas
+        # players + npc
         self.players_list = []
-        self.territorios_dict = {} #Dicionario de territórios para ser acessado pela partida
+        self.territorios_dict = {}  # Dicionario de territórios para ser acessado pela partida
         self.over = False
 
         # ---------------------------------------------------------------------------------------
-        # essa parte deveria checar se existe um n�mero suficiente de jogadores para jogar o jogo
-        # qualquer coisa acerta esse if
-        if( self.n_players == 1 ):
-            print("A partida não pode ser criada")
-            del self
-            return
-        
-        #---------------------------------------------------------------------------------------
+        # # essa parte deveria checar se existe um n�mero suficiente de jogadores para jogar o jogo
+        # # qualquer coisa acerta esse if
+        # if (self.n_players == 1):
+        #     print("A partida não pode ser criada")
+        #     del self
+        #     return
+
+        # ---------------------------------------------------------------------------------------
         # embaralhar as cartas objetivo e distribuí-las(várias maneiras de fazer isso)Ainda não implementei
         goal_cards = CartaObj.cartaObj.gerar_cartas()
-        random.shuffle( goal_cards )
-        self.goal_cards = goal_cards[ : self.n_players ]
+        random.shuffle(goal_cards)
+        self.goal_cards = goal_cards[: self.n_players]
 
-        # aqui seria a escolha das cores
-        cores_disponiveis = [ True ]*len( CORES )
-        cores_escolhidas = []
-        for x in range(n_humans):
-            while True :
+        # # aqui seria a escolha das cores
+        # cores_disponiveis = [True] * len(CORES)
+        # cores_escolhidas = []
+        # for x in range(n_humans):
+        #     while True:
+        #         print(f"Jogador {x} escolha a sua cor:")
+        #         for i, cor in enumerate(CORES):
+        #             if not cores_disponiveis[i]:
+        #                 continue
+        #             print(f"{i} -> {cor}")
+        #         print()
+        #
+        #         try:
+        #             cor_escolhida = int(input("Digite um número: "))
+        #         except ValueError:
+        #             print("Entrada Inválida")
+        #             continue
+        #
+        #         if cor_escolhida > len(CORES) or cor_escolhida < 0:
+        #             print("Entrada Inválida")
+        #             continue
+        #
+        #         if not cores_disponiveis[cor_escolhida]:
+        #             print("Essa cor ja foi escolhida")
+        #             continue
+        #
+        #         cores_disponiveis[cor_escolhida] = False
+        #         Cor = CORES[cor_escolhida]
+        #         cores_escolhidas.append(Cor)
+        #         break
 
-                print( f"Jogador {x} escolha a sua cor:")
-                for i , cor in enumerate( CORES ):
-                    if not cores_disponiveis[ i ]:
-                        continue
-                    print( f"{i} -> {cor}")
-                print()
-
-                try:
-                    cor_escolhida = int( input("Digite um número: ") )
-                except ValueError:
-                    print( "Entrada Inválida" )
-                    continue
-
-                if cor_escolhida > len( CORES ) or cor_escolhida < 0:
-                    print( "Entrada Inválida" )
-                    continue
-
-                if not cores_disponiveis[ cor_escolhida ]:
-                    print( "Essa cor ja foi escolhida" )
-                    continue
-                
-                cores_disponiveis[ cor_escolhida ] = False
-                Cor = CORES[ cor_escolhida ]
-                cores_escolhidas.append( Cor )
-                break
-        
         # ---------------------------------------------------------------------
         # criar jogadores
         self.v_player = []
-        for cor , card in zip( cores_escolhidas , self.goal_cards[ :self.n_humans ] ):
-            self.v_player.append( Player.player( card, True, cor, False ) )
+        for cor, card in zip(self.cores_escolhidas, self.goal_cards[:self.n_humans]):
+            self.v_player.append(Player.player(card, True, cor, False))
 
         # ---------------------------------------------------------------------
         # inicializar npc
         self.npcs = []
         if self.n_npcs != 0:
-            remaining_colors = [ cor for i , cor in enumerate( CORES ) if cores_disponiveis[ i ] ]
-            npc_goal_cards   = self.goal_cards[ self.n_humans: ]
-            for cor , card in zip( remaining_colors , npc_goal_cards ):
-                self.npcs.append( Player.player( card, True, cor, True )  ) 
+            remaining_colors = [cor for i, cor in enumerate(CORES) if self.cores_disponiveis[i]]
+            npc_goal_cards = self.goal_cards[self.n_humans:]
+            for cor, card in zip(remaining_colors, npc_goal_cards):
+                self.npcs.append(Player.player(card, True, cor, True))
 
-        # ---------------------------------------------------------------------
+                # ---------------------------------------------------------------------
         # distribuicao de territorios 
         self.territorios_dict, territorios = territorio.get_territorios()
-        random.shuffle( territorios )
-        players = deque( self.v_player + self.npcs )    # Total de membros na partida
+        random.shuffle(territorios)
+        players = deque(self.v_player + self.npcs)  # Total de membros na partida
         for terr in territorios:
-            player = players[ 0 ]
+            player = players[0]
             terr.player = player
             terr.tropas = 1
-            player.territorios.append( terr )
+            player.territorios.append(terr)
             players.rotate()
-        
-        #Lista geral (players + npc)
+
+        # Lista geral (players + npc)
         self.players_list = list(players)
 
-        #Inicio do jogo
+        # Inicio do jogo
         for player in (self.players_list):
-            
+
             if not player.is_npc:
-                print(player.cor) 
-                #Para debug
+                print(player.cor)
+                # Para debug
                 player.print_territories_names()
 
                 player.reserves = player.get_round_reserve()
                 self.allocate_reserve_loop(player)
             else:
-                #Realizar Lógica da IA para colocar tropas
+                # Realizar Lógica da IA para colocar tropas
                 pass
-        
 
-        #Após preparação, iniciar game_loop
+        # Após preparação, iniciar game_loop
         self.game_loop()
-            
-    #Fase inicial da rodada
+
+    # Fase inicial da rodada
     def fase_de_tropas(self, player):
         print("FASE DE TROPAS PLAYER: " + str(player.cor))
         print("-------------------------------------------------------------")
         if not player.is_npc:
-            #Para debug
+            # Para debug
             player.print_territories_names()
 
-            #Receber exercitos
+            # Receber exercitos
             player.reserves = player.get_round_reserve()
 
-            #Troca de cartas
-            #Realizar troca de cartas de território, se possivel, para aumentar as reservas
+            # Troca de cartas
+            # Realizar troca de cartas de território, se possivel, para aumentar as reservas
 
-            #Colocar exercitos nos territórios
+            # Colocar exercitos nos territórios
             self.allocate_reserve_loop(player)
         else:
-            #Realizar Lógica da IA para colocar tropas
-            pass 
-    
-    #Função base para o combate , retorna verdadeiro apenas se o território alvo não tiver mais tropas para defender
+            # Realizar Lógica da IA para colocar tropas
+            pass
+
+            # Função base para o combate , retorna verdadeiro apenas se o território alvo não tiver mais tropas para defender
+
     def attack_territory(self, origin, target, amount):
         if target not in origin.vizinhos:
             print("Territórios não fazem fronteira")
             return
-        
+
         if amount < 2:
             print("são necessários mais tropas para atacar")
             return
-        
-        #Realizar combate
+
+        # Realizar combate
         attack_dices = partida.get_actual_combat_troops(amount, False)
         defense_dices = partida.get_actual_combat_troops(target.tropas, True)
 
-        attack_dice_list = [random.randint(1,6) for i in range(attack_dices)]
-        defense_dice_list = [random.randint(1,6) for i in range(defense_dices)]
+        attack_dice_list = [random.randint(1, 6) for i in range(attack_dices)]
+        defense_dice_list = [random.randint(1, 6) for i in range(defense_dices)]
 
         attack_dice_list.sort()
         defense_dice_list.sort()
 
-        #Guardar defesas
+        # Guardar defesas
         attack_losses = 0
         defense_losses = 0
 
@@ -166,46 +168,45 @@ class partida:
         print(defense_dice_list)
 
         for i in range(min(len(attack_dice_list), len(defense_dice_list))):
-            #São comparados com a regra do war
+            # São comparados com a regra do war
             if attack_dice_list[i] > defense_dice_list[i]:
-                #Esse dado de ataque ganhou
+                # Esse dado de ataque ganhou
                 defense_losses += 1
             else:
                 attack_losses += 1
-        
 
         print("Atacante sofreu: " + str(attack_losses))
         print("Defensor sofreu: " + str(defense_losses))
 
-        #Atualizar tropas em cada território
+        # Atualizar tropas em cada território
         origin.tropas -= attack_losses
         target.tropas -= defense_losses
-        
-        #Testar se território foi capturado
+
+        # Testar se território foi capturado
         if target.tropas == 0:
             return True
         else:
             return False
 
-    #fase de combate
+    # fase de combate
     def fase_combate(self, player):
         print("FASE DE COMBATE PLAYER: " + str(player.cor))
         print("-------------------------------------------------------------")
         if not player.is_npc:
-            while(player.available_for_attack()):
-                #Receber origem do ataque
-                #Receber alvo do ataque
-                #Receber quantidade de tropas utilizadas
+            while (player.available_for_attack()):
+                # Receber origem do ataque
+                # Receber alvo do ataque
+                # Receber quantidade de tropas utilizadas
                 print("Comece o ataque")
-                #Para debug
+                # Para debug
                 player.print_territories_names()
 
-                #Mudar depois para receber com clique do mouse os inputs
+                # Mudar depois para receber com clique do mouse os inputs
                 origin_id = int(input("ID do território de origem: "))
                 origin_territory = self.territorios_dict[origin_id]
 
                 origin_territory.print_neighbors_names()
-                
+
                 target_id = int(input("ID do território alvo do ataque: "))
 
                 target_territory = self.territorios_dict[target_id]
@@ -213,64 +214,62 @@ class partida:
                 amount = int(input("Quantidade de tropas para se utilizar: "))
                 if amount > origin_territory.tropas:
                     print("Numero de tropas indisponiveis nesse territorio")
-                    #Passa para próxima iteração do loop
+                    # Passa para próxima iteração do loop
                     continue
 
                 if target_territory.player == origin_territory.player:
                     print("Esse território já é seu")
-                    #Passa para próxima iteração do loop
+                    # Passa para próxima iteração do loop
                     continue
 
                 target_has_no_troops_left = self.attack_territory(origin_territory, target_territory, amount)
 
-                #Ganhou o território
+                # Ganhou o território
                 if target_has_no_troops_left:
                     print("território capturado!")
                     target_territory.player = player
-                    #Para debug
+                    # Para debug
                     print("Tropas no território de origem: " + str(origin_territory.tropas))
-                    #Modificar input para clique de
+                    # Modificar input para clique de
                     amount_to_move = int(input("Digite quantos exércitos você deseja mover para o novo território: "))
                     player.move_army_terr(origin_territory, target_territory, amount_to_move)
-                    
 
-                #Testar se quer parar de atacar
-                #Modificar input para clique do mouse
+                # Testar se quer parar de atacar
+                # Modificar input para clique do mouse
                 if input("Se deseja parar de atacar digite 0: ") == '0':
                     print("parou?")
                     break
         else:
-            #Realizar Lógica da IA para combate
+            # Realizar Lógica da IA para combate
             pass
-        
 
-    #fase de movimentação
+    # fase de movimentação
     def fase_movimento(self, player):
         print("FASE DE MOVIMENTAÇÃO PLAYER: " + str(player.cor))
         print("-------------------------------------------------------------")
         if not player.is_npc:
             is_mooving = True
-            while(is_mooving):
-                #Modificar input para clique do mouse
-                #Receber origem 
-                #Receber alvo
-                #Receber quantidade de tropas utilizadas
+            while (is_mooving):
+                # Modificar input para clique do mouse
+                # Receber origem
+                # Receber alvo
+                # Receber quantidade de tropas utilizadas
 
                 print("Comece a movimentação")
-                #Para debug
+                # Para debug
                 player.print_territories_names()
 
-                #Mudar depois para receber com clique do mouse os inputs
+                # Mudar depois para receber com clique do mouse os inputs
                 origin_id = int(input("ID do território de origem: "))
                 origin_territory = self.territorios_dict[origin_id]
 
                 origin_territory.print_neighbors_names()
-                
+
                 target_id = int(input("ID do território alvo:  "))
 
                 target_territory = self.territorios_dict[target_id]
 
-                #Modificar para input pela tela
+                # Modificar para input pela tela
                 amount = int(input("Quantidade de tropas para mover"))
 
                 player.move_army_terr(origin_territory, target_territory, amount)
@@ -280,34 +279,33 @@ class partida:
 
 
         else:
-            #Implementar lógica de IA para fase de movimentação
+            # Implementar lógica de IA para fase de movimentação
             pass
-         
-    
 
-    #Helper functions
+    # Helper functions
 
-    #Função para retornar numero de dados em combate
+    # Função para retornar numero de dados em combate
     def get_actual_combat_troops(amount, defesa):
         if amount < 4:
-            #Se forem até 3 tropas atacando:
-            if defesa : return amount
-            else : return amount - 1
+            # Se forem até 3 tropas atacando:
+            if defesa:
+                return amount
+            else:
+                return amount - 1
         else:
             return 3
 
     def get_allocate_reserve_input(self):
-        #Mudar essa função depois para receber inputs por clique no mapa e input na tela
+        # Mudar essa função depois para receber inputs por clique no mapa e input na tela
         id = int(input("ID do territorio: "))
         amount = int(input("Escolha a quantidade de tropas para colocar: "))
 
         return self.territorios_dict[id], amount
 
     def allocate_reserve_loop(self, player):
-        while(player.reserves != 0):
+        while (player.reserves != 0):
             territory, amount = self.get_allocate_reserve_input()
             player.allocate_reserve(territory, amount)
-
 
     def game_loop(self):
         rodada = 1
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tracemalloc import start
 
 import pygame
 
+from players_selection import PlayersSelection
 from PPlay.gameimage import *
 from PPlay.mouse import *
 from PPlay.window import *
@@ -18,22 +19,32 @@ screen = pygame.display.set_mode((1600, 900))
 
 main_menu = MainMenu()
 game = Game()
+ps = PlayersSelection()
 
-while globals.GAME_STATE != 5:
+while globals.GAME_STATE != 50:
+    pygame.event.get()
     if globals.GAME_STATE == 0:
         main_menu.run(screen)
-    if main_menu.start_game_btn.draw(screen):
-        globals.GAME_STATE = 1
-        #INICIO DA PARTIDA
-        p = Partida.partida(2, 6)
-        
-    if main_menu.exit_game_btn.draw(screen):
-        pygame.quit()
-        sys.exit()
+
+        if main_menu.start_game_btn.draw(screen):
+            globals.GAME_STATE = 1
+
+        if main_menu.exit_game_btn.draw(screen):
+            pygame.quit()
+            sys.exit()
 
     elif globals.GAME_STATE == 1:
-        game.run(screen)
+        while globals.IN_PLAYER_SELECT is True:
+            try:
+                ps.run(screen)
+            except AttributeError:
+                sys.exit()
+
+            pygame.display.update()
+
+    elif globals.GAME_STATE == 2:
+        print('entrou')
+        Partida.partida(ps.n_players, ps.cores_disponiveis, ps.cores_escolhidas)
 
     pygame.display.update()
     mainClock.tick(60)
-
